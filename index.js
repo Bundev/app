f44dad75f941ff0c0ec8418161c445a993ba96e4
Ivan <bundev95@gmail.com>
2026-05-30 18:52:38 +0300

    Автосохранение 2026-05-30 18:52:38

--- index.js
+++ index.js
@@ -36,14 +36,17 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 app.get('/add', (req, res) => {
-    res.render('add', { title: 'Add Page' });
+    // res.render('add', { title: 'Add Page' });
+
+
 });
 
 
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
 
 
 
