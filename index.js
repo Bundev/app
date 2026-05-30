b64052ebdb4d204927461f04f02edb9816b7e254
Ivan <bundev95@gmail.com>
2026-05-30 18:54:35 +0300

    Автосохранение 2026-05-30 18:54:35

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ app.get('/add', (req, res) => {
 
 
 
-// app.listen(port, () => {
-//     console.log(`Server started on port ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
 
 
 
